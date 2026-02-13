0abbd19bcb8cdbed79605ea6fb027085af83d444
tonijiny8 <tonijinyang@gmail.com>
2026-02-13 07:06:43 +0100

    Update app.js
    been working on a repository with no permissions, therefore copied and pasted the code from the other

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,58 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  let cover = "<div class='cover'></div>";
+  if (variables.includeCover === true && variables.background) {
+    cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  }
+
+  const fullName = [variables.name, variables.lastName].filter(Boolean).join(" ");
+  const location = [variables.city, variables.country].filter(Boolean).join(", ");
+
+  let twitterLi = "";
+  if (variables.twitter) {
+    twitterLi = `<li><a href="https://twitter.com/${variables.twitter}" target="_blank" rel="noreferrer"><i class="fab fa-twitter"></i></a></li>`;
+  }
+
+  let githubLi = "";
+  if (variables.github) {
+    githubLi = `<li><a href="https://github.com/${variables.github}" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></li>`;
+  }
+
+  let linkedinLi = "";
+  if (variables.linkedin) {
+    linkedinLi = `<li><a href="https://linkedin.com/${variables.linkedin}" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a></li>`;
+  }
+
+  let instagramLi = "";
+  if (variables.instagram) {
+    instagramLi = `<li><a href="https://instagram.com/${variables.instagram}" target="_blank" rel="noreferrer"><i class="fab fa-instagram"></i></a></li>`;
+  }
+
+  let socialHTML = "";
+  if (twitterLi || githubLi || linkedinLi || instagramLi) {
+    socialHTML = `<ul class="${variables.socialMediaPosition}">
+      ${twitterLi}
+      ${githubLi}
+      ${linkedinLi}
+      ${instagramLi}
+    </ul>`;
+  }
+
+  document.querySelector("#widget_content").innerHTML = `
+    <div class="widget">
+      ${cover}
+      <img src="${variables.avatarURL}" class="photo" />
+
+      <h1>${fullName}</h1>
+      <h2>${variables.role || ""}</h2>
+      <h3>${location}</h3>
+
+      ${socialHTML}
+    </div>
+  `;
 }
 
 /**
